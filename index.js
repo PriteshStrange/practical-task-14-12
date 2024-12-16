f4836178e385359d3b7079126eafe042784f977a
PriteshStrange <priteshrana960@gmail.com>
2024-12-16 20:17:04 +0530

    minor-changes

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 
 app.use(errorMiddleware);
 
-const server = app.listen(port, () => {
+app.listen(port, () => {
     try {
         console.log(`Server is listing on the ${port}`)
     } catch (error) {
